1bc510fafd341ac85d931bdf53bf2e6587c5cb3a
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-12-31 10:57:46 -0200

    Adicionado som durante disparo (testing)

--- Jogo/src/main/java/br/ufscar/dc/cg/jogo/GameGUI.java
+++ Jogo/src/main/java/br/ufscar/dc/cg/jogo/GameGUI.java
@@ -1,12 +1,28 @@
 package br.ufscar.dc.cg.jogo;
 
+import static br.ufscar.dc.cg.jogo.audio.IOUtil.ioResourceToByteBuffer;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.util.List;
 import org.lwjgl.BufferUtils;
 import static org.lwjgl.glfw.GLFW.*;
 import org.lwjgl.glfw.*;
+import org.lwjgl.openal.AL;
+import org.lwjgl.openal.ALC;
+import static br.ufscar.dc.cg.jogo.audio.OpenALInfo.checkALCError;
+import static br.ufscar.dc.cg.jogo.audio.OpenALInfo.checkALError;
+import java.nio.ShortBuffer;
+import static org.lwjgl.openal.AL10.*;
+import static org.lwjgl.openal.ALC10.*;
+import static org.lwjgl.openal.ALC11.*;
+import org.lwjgl.openal.ALCCapabilities;
+import org.lwjgl.openal.ALUtil;
+import static org.lwjgl.openal.EXTThreadLocalContext.alcSetThreadContext;
 import org.lwjgl.opengl.GL;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.stb.STBVorbis.*;
+import org.lwjgl.stb.STBVorbisInfo;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class GameGUI {
@@ -23,6 +39,11 @@ public class GameGUI {
     private static int ROTATION_ORIENTATION = 1;
     private static final double ROTATION_BASE_INCREMENT = 0.5;
 
+    long context;
+    long device;
+    int buffer;
+    int source;
+
     /* Status flags */
     private final boolean[] keyDown = new boolean[GLFW.GLFW_KEY_LAST];
     private boolean spaceKeyDown = false;
@@ -31,6 +52,7 @@ public class GameGUI {
     private double down = 0;
     private boolean shot = false;
     private boolean paint = false;
+    private boolean sound_on = true;
     private double disparo;
     private double colide;
 
@@ -73,6 +95,9 @@ public class GameGUI {
                 if (key == GLFW_KEY_R && action == GLFW_RELEASE) {
                     game.reset_level();
                 }
+                if (key == GLFW_KEY_S && action == GLFW_RELEASE) {
+                    sound_on = !sound_on;
+                }
                 if (key == GLFW_KEY_Q && action == GLFW_RELEASE) {
                     ROTATION_ORIENTATION = -ROTATION_ORIENTATION;
                     ROTATION_INCREMENT += 0.1;
@@ -112,6 +137,93 @@ public class GameGUI {
         // Make the window visible
         glfwShowWindow(window);
 
+        device = alcOpenDevice((ByteBuffer) null);
+        if (device == NULL) {
+            throw new IllegalStateException("Failed to open the default device.");
+        }
+
+        ALCCapabilities deviceCaps = ALC.createCapabilities(device);
+
+        assertTrue(deviceCaps.OpenALC10);
+
+        System.out.println("OpenALC10: " + deviceCaps.OpenALC10);
+        System.out.println("OpenALC11: " + deviceCaps.OpenALC11);
+        System.out.println("caps.ALC_EXT_EFX = " + deviceCaps.ALC_EXT_EFX);
+
+        if (deviceCaps.OpenALC11) {
+            List<String> devices = ALUtil.getStringList(NULL, ALC_ALL_DEVICES_SPECIFIER);
+            if (devices == null) {
+                checkALCError(NULL);
+            } else {
+                for (int i = 0; i < devices.size(); i++) {
+                    System.out.println(i + ": " + devices.get(i));
+                }
+            }
+        }
+
+        String defaultDeviceSpecifier = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
+        assertTrue(defaultDeviceSpecifier != null);
+        System.out.println("Default device: " + defaultDeviceSpecifier);
+
+        context = alcCreateContext(device, (IntBuffer) null);
+        alcSetThreadContext(context);
+        AL.createCapabilities(deviceCaps);
+
+        System.out.println("ALC_FREQUENCY: " + alcGetInteger(device, ALC_FREQUENCY) + "Hz");
+        System.out.println("ALC_REFRESH: " + alcGetInteger(device, ALC_REFRESH) + "Hz");
+        System.out.println("ALC_SYNC: " + (alcGetInteger(device, ALC_SYNC) == ALC_TRUE));
+        System.out.println("ALC_MONO_SOURCES: " + alcGetInteger(device, ALC_MONO_SOURCES));
+        System.out.println("ALC_STEREO_SOURCES: " + alcGetInteger(device, ALC_STEREO_SOURCES));
+
+        buffer = alGenBuffers();
+        checkALError();
+
+        source = alGenSources();
+        checkALError();
+
+        try (STBVorbisInfo info = STBVorbisInfo.malloc()) {
+            ShortBuffer pcm = readVorbis("test.ogg", 32 * 1024, info);
+
+            //copy to buffer
+            alBufferData(buffer, info.channels() == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16, pcm, info.sample_rate());
+            checkALError();
+        }
+
+        //set up source input
+        alSourcei(source, AL_BUFFER, buffer);
+        checkALError();
+
+        //lets loop the sound
+        //alSourcei(source, AL_LOOPING, AL_TRUE);
+        //checkALError();
+    }
+
+    static ShortBuffer readVorbis(String resource, int bufferSize, STBVorbisInfo info) {
+        ByteBuffer vorbis;
+        try {
+            vorbis = ioResourceToByteBuffer(resource, bufferSize);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        IntBuffer error = BufferUtils.createIntBuffer(1);
+        long decoder = stb_vorbis_open_memory(vorbis, error, null);
+        if (decoder == NULL) {
+            throw new RuntimeException("Failed to open Ogg Vorbis file. Error: " + error.get(0));
+        }
+
+        stb_vorbis_get_info(decoder, info);
+
+        int channels = info.channels();
+
+        int lengthSamples = stb_vorbis_stream_length_in_samples(decoder);
+
+        ShortBuffer pcm = BufferUtils.createShortBuffer(lengthSamples);
+
+        pcm.limit(stb_vorbis_get_samples_short_interleaved(decoder, channels, pcm) * channels);
+        stb_vorbis_close(decoder);
+
+        return pcm;
     }
 
     private void loop() {
@@ -152,6 +264,11 @@ public class GameGUI {
             lastShotTime = thisTime;
             shot = true;
             paint = true;
+            //play source 0
+            if (sound_on) {
+                alSourcePlay(source);
+                checkALError();
+            }
         }
 
         if (paint) {
@@ -229,6 +346,15 @@ public class GameGUI {
             t.printStackTrace();
         } finally {
             glfwTerminate();
+            //delete buffers and sources
+            alDeleteSources(source);
+            checkALError();
+
+            alDeleteBuffers(buffer);
+            checkALError();
+            alcDestroyContext(context);
+            alcCloseDevice(device);
+
             glfwSetErrorCallback(null).free();
         }
     }
@@ -287,6 +413,9 @@ public class GameGUI {
             //if (down < colide) {
             if (down < -1) {
                 shot = false; // trocar para colide
+                //stop source 0
+                //alSourceStop(source);
+                //checkALError();
                 /*try {
                     Thread.sleep(10);
                 } catch (InterruptedException ex) {
@@ -304,5 +433,9 @@ public class GameGUI {
 
         Text.drawString("Lados:", 4, 7.5f, 0.45f, 3f);
         Text.drawString(Integer.toString(game.getCount_edges()), 8f, 7.5f, 0.45f, 1f);
+    }
+
+    private void assertTrue(boolean OpenALC10) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
